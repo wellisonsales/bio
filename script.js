8e0f28e98b80af6e1ba21534472758aa5c471e29
Wellison Sales <wellisonsalesbr@gmail.com>
2022-07-22 04:04:46 -0300

    feat: chamando botao com js + detalhes css

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const header = document.querySelector('.header');
 const icon = document.querySelector('.icon');
+const btn = document.querySelector('.btn')
 
 icon.addEventListener('click', () => {
-    header.classList.toggle('dark')
+    header.classList.toggle('dark');
 })
